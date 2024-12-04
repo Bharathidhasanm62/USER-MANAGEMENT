7bb5dc7aded5208de8ebe816c340ec7ad96d54cd
Bharathi <Bharathi@gmail.com>
2024-12-04 16:32:46 +0530

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,44 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./components/Auth/Login";
-// import Signup from "./components/Auth/Signup";
-// import UserDashboard from "./components/User/UserDashboard";
-// import AdminDashboard from "./components/Admin/AdminDashboard";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import { AuthProvider } from "./components/AuthContext";
-// import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import UserDashboard from "./components/User/UserDashboard";
+import AdminDashboard from "./components/Admin/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./components/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 const App: React.FC = () => {
   return (
-    <div>Tets</div>
-    // <Router>
-    //   <AuthProvider>
-    //     <Routes>
-    //       <Route path="/" element={<Login />} />
-    //       <Route path="/login" element={<Login />} />
-    //       <Route path="/signup" element={<Signup />} />
-    //       <Route
-    //         path="/user-dashboard"
-    //         element={
-    //           <ProtectedRoute
-    //             component={UserDashboard}
-    //             allowedRoles={["user"]}
-    //           />
-    //         }
-    //       />
-    //       <Route
-    //         path="/admin-dashboard"
-    //         element={
-    //           <ProtectedRoute
-    //             component={AdminDashboard}
-    //             allowedRoles={["admin"]}
-    //           />
-    //         }
-    //       />
-    //     </Routes>
-    //     <Toaster />
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/user-dashboard"
+            element={
+              <ProtectedRoute
+                component={UserDashboard}
+                allowedRoles={["user"]}
+              />
+            }
+          />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <ProtectedRoute
+                component={AdminDashboard}
+                allowedRoles={["admin"]}
+              />
+            }
+          />
+        </Routes>
+        <Toaster />
 
-    //   </AuthProvider>
-    // </Router>
+      </AuthProvider>
+    </Router>
   );
 };
 
